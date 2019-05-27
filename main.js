17c2d9507d64e40bccf8fbb00d62459e1fe08a49
CarlaDif <carlita4692@gmail.com>
2019-05-27 15:27:22 +0200

    Visualizzazione in console dei film cercati

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   $('button').click(function(){
     //leggo il valore del placeholder
     var titolo_film = $('input').val();
-    console.log(titolo_film);
     //richiamo la funzione ajax
     $.ajax ({
       'url': 'https://api.themoviedb.org/3/search/movie',
@@ -13,8 +12,17 @@ $(document).ready(function(){
         'language': 'it'
       },
       'success': function (data, status){
-        console.log(data);
-        console.log(status);
+        //visualizzo l'array "elenco_film", contenente gli oggetti "film"
+        var elenco_film = data.results;
+        //ciclo all'interno dell'array composto dagli oggetti/film
+        for (var i = 0; i < elenco_film.length; i++) {
+          var film = elenco_film[i];
+          var titolo = film.title;
+          var titolo_originale = film.original_title;
+          var lingua = film.original_language;
+          var voto = film.vote_average;
+          console.log('Titolo: ' + titolo + ' Titolo originale: ' + titolo_originale + ' Lingua: ' + lingua +  ' Voto: ' + voto );
+        }
       },
       'error': function (){
         console.log('qualcosa non va');
